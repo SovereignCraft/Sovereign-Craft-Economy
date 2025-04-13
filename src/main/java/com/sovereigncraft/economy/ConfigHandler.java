2b8799abb7a60378d4079ad2fc71d384252ff50a
CappyTech <133281784+CappyTech@users.noreply.github.com>
2025-04-13 23:45:36 +0100

    Update ConfigHandler.java
    Made some indent changes to make it look uniform.

--- src/main/java/com/sovereigncraft/economy/ConfigHandler.java
+++ src/main/java/com/sovereigncraft/economy/ConfigHandler.java
@@ -8,27 +8,32 @@ public class ConfigHandler {
     private static FileConfiguration getConfig() {
         return SCEconomy.getInstance().getConfig();
     }
-    public static String getHost() { return getConfig().getString("host"); }
-    public static String getPubHost() { return getConfig().getString("pubhost"); }
-
+    public static String getHost() { 
+        return getConfig().getString("host");
+    }
+    public static String getPubHost() { 
+        return getConfig().getString("pubhost");
+    }
     public static String getPort() {
         return getConfig().getString("port");
     }
-
     public static String getAPIKey() {
         return getConfig().getString("APIKey");
     }
-
     public static String getAdminUser() {
         return getConfig().getString("AdminUser");
     }
-
-    public static UUID getServerUUID() { return UUID.fromString(getConfig().getString("ServerUUID")); }
-
+    public static UUID getServerUUID() { 
+        return UUID.fromString(getConfig().getString("ServerUUID"));
+    }
     public static String getAdminKey() {
         return getConfig().getString("AdminKey");
     }
-    public static Boolean getLNAddress() { return (getConfig().getBoolean("lnaddress")); }
+
+    public static Boolean getLNAddress() {
+        return (getConfig().getBoolean("lnaddress"));
+    }
+
     public static Double getStartingBalance() {
         return getConfig().getDouble("startingBalance");
     }
